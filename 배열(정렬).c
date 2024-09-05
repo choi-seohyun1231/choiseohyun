ba707fd88eb4ee94a0e8efd6a70a779e59cb9b54
choi-seohyun1231 <lovelychoi08@naver.com>
2024-09-05 11:52:23 +0900

    Add files via upload

---
+++ 배열(정렬).c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 100
+int main()
+{
+    int number[SIZE];
+    int most;
+    int temp;
+    srand(time(NULL));
+    for(int i=0; i<SIZE; i++){
+        number[i]=(rand()%10000)+1;
+    }
+    for(int i=0; i<SIZE; i++){
+        most=i;
+        for(int j= i+1; j<SIZE; j++){
+           if(number[most]<number[j])most=j; 
+        }
+        temp=number[i];
+        number[i]=number[most];
+        number[most]=temp;
+    }
+    for(int i=0; i<SIZE; i++){
+       printf("%5d\t", number[i]);
+       if((i+1)%10==0)printf("\n");
+    }
+    return 0;
+}
